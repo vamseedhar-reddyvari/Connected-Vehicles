cfe4317ce08c86dfbbcbcfd32064483371344baa
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2016-02-12 11:24:16 -0600

    added different marker for vehicles

--- Mapbox/js/rse-script.js
+++ Mapbox/js/rse-script.js
@@ -1,6 +1,6 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGl2YW1zZWUiLCJhIjoiY2lraGFyMHRiMDFjeXYwa21mdWYxb2t0NiJ9.DaVAaeEszhOTuPzxZyCFwA';
 var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([42.268138, -83.79706], 13);
+    .setView([42.288138, -83.69706], 13);
 
 var geojson;
 
@@ -15,5 +15,21 @@ $.getJSON('../data/rse-geo.json', function(response){
 //});
 $.getJSON('../data/rse-data-locations.json', function(response){
        geojson= response;
-       L.geoJson(geojson, { style: L.mapbox.simplestyle.style }).addTo(map);
+       L.geoJson(geojson, {
+         style:L.mapbox.simplestyle.style }).addTo(map);
 });
+// icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}
+// style: L.mapbox.simplestyle.style
+var myIcon = L.icon({
+	iconUrl: 'my-icon.png',
+	iconRetinaUrl: 'my-icon@2x.png',
+	iconSize: [38, 95],
+	iconAnchor: [22, 94],
+	popupAnchor: [-3, -76],
+	shadowUrl: 'my-icon-shadow.png',
+	shadowRetinaUrl: 'my-icon-shadow@2x.png',
+	shadowSize: [68, 95],
+	shadowAnchor: [22, 94]
+});
+
+L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
--- scripts/rse-locations.py
+++ scripts/rse-locations.py
@@ -5,20 +5,17 @@ import geojson as geo
 Center = (-83.79706,42.268138)
 Center_Point = geo.Point(Center)
 
-
-
-
-
-
-
-
 def get_locations(filename):
     opened_file = open(filename)
     features_list = []
     for line in opened_file:
         line_contents = line.split(',')
         (longitude,latitude) = (float(line_contents[7])/10000000,float(line_contents[6])/10000000 )
-        f = geo.Feature(geometry=geo.Point( (longitude,latitude)),properties='RX Pnt')
+        f = geo.Feature(geometry=geo.Point( (longitude,latitude)), \
+            properties = {'type':'Vehicle Location', \
+            "marker-symbol": "car",\
+            "marker-size": "medium",\
+            "marker-color": "#f44"} )
         features_list.append(f)
         #break
     F = geo.FeatureCollection(features_list)
@@ -30,7 +27,6 @@ def get_locations(filename):
     json_file  = open('../Mapbox/data/rse-data-locations.json','w')
     json_file.write(geo_data_str)
     json_file.close()
-
 
 
 
