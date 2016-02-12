40794a06c4944c580ab39155d46430e3fd0c2baf
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2016-02-12 12:09:15 -0600

    added RSE locations from geometry which are not displaying properly

--- Mapbox/js/rse-script.js
+++ Mapbox/js/rse-script.js
@@ -1,35 +1,20 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGl2YW1zZWUiLCJhIjoiY2lraGFyMHRiMDFjeXYwa21mdWYxb2t0NiJ9.DaVAaeEszhOTuPzxZyCFwA';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([42.288138, -83.69706], 13);
+var map = L.mapbox.map('map', 'mapbox.streets').setView([42.288138, -83.69706], 13);
 
 var geojson;
 
-$.getJSON('../data/rse-geo.json', function(response){
+$.getJSON('../data/vehicle-locations.json', function(response){
        geojson= response;
-       L.geoJson(geojson, { style: L.mapbox.simplestyle.style }).addTo(map);
+      //  L.geoJson(geojson, { style:L.mapbox.simplestyle.style }).addTo(map);
+
+       var myLayer = L.mapbox.featureLayer().addTo(map);
+       myLayer.setGeoJSON(geojson);
 });
 
-//$.getJSON('../data/center-point.json', function(response){
-//       geojson= response;
-//       L.geoJson(geojson, { style: L.mapbox.simplestyle.style }).addTo(map);
-//});
 $.getJSON('../data/rse-data-locations.json', function(response){
        geojson= response;
-       L.geoJson(geojson, {
-         style:L.mapbox.simplestyle.style }).addTo(map);
-});
-// icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}
-// style: L.mapbox.simplestyle.style
-var myIcon = L.icon({
-	iconUrl: 'my-icon.png',
-	iconRetinaUrl: 'my-icon@2x.png',
-	iconSize: [38, 95],
-	iconAnchor: [22, 94],
-	popupAnchor: [-3, -76],
-	shadowUrl: 'my-icon-shadow.png',
-	shadowRetinaUrl: 'my-icon-shadow@2x.png',
-	shadowSize: [68, 95],
-	shadowAnchor: [22, 94]
-});
+      //  L.geoJson(geojson, { style:L.mapbox.simplestyle.style }).addTo(map);
 
-L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
+       var myLayer = L.mapbox.featureLayer().addTo(map);
+       myLayer.setGeoJSON(geojson);
+});
--- scripts/rse-locations.py
+++ scripts/rse-locations.py
@@ -5,17 +5,14 @@ import geojson as geo
 Center = (-83.79706,42.268138)
 Center_Point = geo.Point(Center)
 
-def get_locations(filename):
+def get_locations(filename,lat_idx,long_idx, write_filename,point_properties):
     opened_file = open(filename)
     features_list = []
     for line in opened_file:
         line_contents = line.split(',')
-        (longitude,latitude) = (float(line_contents[7])/10000000,float(line_contents[6])/10000000 )
+        (longitude,latitude) = (float(line_contents[lat_idx])/10000000,float(line_contents[long_idx])/10000000 )
         f = geo.Feature(geometry=geo.Point( (longitude,latitude)), \
-            properties = {'type':'Vehicle Location', \
-            "marker-symbol": "car",\
-            "marker-size": "medium",\
-            "marker-color": "#f44"} )
+            properties = point_properties )
         features_list.append(f)
         #break
     F = geo.FeatureCollection(features_list)
@@ -24,11 +21,13 @@ def get_locations(filename):
 
 
     # Write into file
-    json_file  = open('../Mapbox/data/rse-data-locations.json','w')
+    json_file  = open('../Mapbox/data/'+write_filename,'w')
     json_file.write(geo_data_str)
     json_file.close()
 
 
 
 
-get_locations('../../Transportation-Data/Data/RSE_BSM/RSE_BSM_trimmed.csv')
+# get_locations('../../Transportation-Data/Data/RSE_BSM/RSE_BSM_trimmed.csv')
+get_locations('../BSM-RSE.csv',7,6,'vehicle-locations.json',{'title':'Vehicle Location',  "marker-symbol": "car", "marker-size": "medium", "marker-color": "#09f"})
+get_locations('../Geometry.csv',4,3,'rse-data-locations.json',{'title':'RSE Location',  "marker-symbol": "commercial", "marker-size": "large", "marker-color": "#f83"})
